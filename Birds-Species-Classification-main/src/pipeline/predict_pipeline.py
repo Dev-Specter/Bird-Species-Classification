a50cc218cb99ed674fe61bf5eef7e05f2b9753f4
Abhinav Singh <abhinav.singh16122003@gmail.com>
2024-09-13 04:15:22 +0530

    Add files via upload

---
+++ Birds-Species-Classification-main/src/pipeline/predict_pipeline.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+import os
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import requests
+from io import BytesIO
+import numpy as np
+from src.exception import CustomException
+from src.logger import logging
+
+class predict_species:
+    # Load the saved model
+    model = tf.keras.models.load_model(os.path.join(os.getcwd(),r'model\final_model.h5'))
+
+    # get the class names
+    class_names = os.listdir(os.path.join(os.getcwd(),r'100-bird-species\train'))
+
+    @classmethod
+    def predict_pipeline(cls,image_path):
+        try:
+            logging.info("Starting the prediction pipeline")
+            # Define the input image size
+            input_shape = (224, 224)
+            
+            logging.info("Reading the image")
+            # load image data
+            img = load_img(image_path, target_size=input_shape)
+            
+            # convert image to array
+            x = img_to_array(img)
+            
+            logging.info("Preprocessing the image")
+            # preprocess the image using efficientnet preprocessor
+            x = preprocess_input(x)
+
+            logging.info("Making the prediction")
+            # Make the prediction
+            preds = cls.model.predict(np.array([x]))
+
+            # get the index of the predicted class
+            index = np.argmax(preds)
+
+            logging.info("Getting the predicted class name")
+            # get the predicted class name
+            predicted_class = cls.class_names[index]
+
+            logging.info("Predicted the class successfully")
+            
+            return predicted_class
+        
+        except Exception as e:
+            logging.info("Error in predicting the class in predict_pipeline")
+            raise CustomException(e)
+
+    @classmethod
+    def predict_pipeline_url(cls,image_url):
+        try:
+            logging.info("Starting the prediction pipeline from URL")
+
+            logging.info("Getting the image from the url")
+            # get the image from the url
+            response = requests.get(image_url)
+
+            # Define the input image size
+            input_shape = (224, 224)
+            
+            logging.info("Reading the image")
+            # load image data
+            img = load_img(BytesIO(response.content), target_size=input_shape)
+            
+            # convert image to array
+            x = img_to_array(img)
+            
+            logging.info("Preprocessing the image")
+            # preprocess the image using efficientnet preprocessor
+            x = preprocess_input(x)
+
+            logging.info("Making the prediction")
+            # Make the prediction
+            preds = cls.model.predict(np.array([x]))
+
+            # get the index of the predicted class
+            index = np.argmax(preds)
+
+            logging.info("Getting the predicted class name")
+            # get the predicted class name
+            predicted_class = cls.class_names[index]
+
+            logging.info("Predicted the class successfully")
+            
+            return predicted_class
+        
+        except Exception as e:
+            logging.info("Error in predicting the class in predict_pipeline")
+            raise CustomException(e)
